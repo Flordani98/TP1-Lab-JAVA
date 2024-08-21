afbb9d14fe33b8286d6497ebc70e6fc3bea59895
Florencia <flordani98@gmail.com>
2024-08-21 01:56:11 -0300

    agrega atributo nombre a Enum TipoEnvase

--- src/Enums/TipoEnvase.java
+++ src/Enums/TipoEnvase.java
@@ -1,5 +1,18 @@
 package Enums;
 
 public enum TipoEnvase {
-    LATA, FRASCO, TETRA_PACK, BLISTER, BOTELLA_VIDRIO, BOTELLA_PLASTICO, ENVASE_FLEXIBLE, ENVASE_PAPEL
+    LATA("Lata"),
+    FRASCO("Frasco"),
+    TETRA_PACK("Tetra Pack"),
+    BLISTER("Blister"),
+    BOTELLA_VIDRIO("Botella de vidrio"),
+    BOTELLA_PLASTICO("Botella de plástico"),
+    ENVASE_FLEXIBLE("Envase flexible"),
+    ENVASE_PAPEL("Envase de papel");
+
+    private final String nombre;
+
+    TipoEnvase(String nombre) { this.nombre = nombre; }
+
+    public String getNombre() { return this.nombre; }
 }
